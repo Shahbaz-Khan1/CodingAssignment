1ebac5a864d3bd9e338f7755e3a6deb2ddf9e210
Md Shahbaz Khan <shahbazkhan85076@gmail.com>
2023-10-22 14:43:47 +0530

    JavaAssignment

---
+++ Pangram.java
@@ -0,0 +1,38 @@
+package com.assingment;
+
+
+
+public class Pangram {
+
+	public static void main(String[] args) {
+
+  String str = "The five boxing wizards jump quickly ";
+   String str1 = str.toLowerCase();
+   int count=0;
+
+	if(str1.length()<26) {
+		System.out.println("Not a Pangram");
+	}
+	
+	else {
+		
+		for(char ch='a';ch<='z';ch++) {
+			
+			if(str1.indexOf(ch)<0) {
+				System.out.println("Not a Pangram");
+				break;
+			}
+			
+			count++;
+			
+		
+		}
+		
+		}
+	 if(count>=26) {
+	System.out.println("pangram sentence");
+		
+	}
+}
+
+}
