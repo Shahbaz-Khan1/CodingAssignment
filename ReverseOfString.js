f99d1aa01c87b99fe7f592adf8d1d8af7344cb25
Md Shahbaz Khan <shahbazkhan85076@gmail.com>
2023-10-22 14:16:42 +0530

    javaScriptAssignment

---
+++ ReverseOfString.js
@@ -0,0 +1,14 @@
+
+let str = "Wlecome to javaScript";
+let reverse = "";
+for(let i=str.length-1;i>=0;i--)
+{
+   reverse +=str[i];
+}
+
+let str1 = reverse.split(" ");
+let reverse1="";
+for(let j=str1.length-1;j>=0;j--){
+    reverse1 += str1[j] + " ";
+}
+console.log(reverse1);
